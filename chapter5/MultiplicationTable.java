c4be8219dade231f91002a0394212dcb7f733714
Toulouse2008 <frank.du2015@gmail.com>
2016-10-05 16:15:25 -0400

    Listing 5.7 Multiplication table

---
+++ chapter5/MultiplicationTable.java
@@ -0,0 +1,21 @@
+/** Listing 5.7 Multiplication table  */
+public class MultiplicationTable {
+  public static void main(String[] args) {
+
+    System.out.println("\t\tMultiplication Table");
+
+    System.out.print("    ");
+    for(int j = 1; j <= 9; j++){
+      System.out.print("   " + j);
+    }
+    System.out.println("\n-----------------------------------------");
+    // Display the table body
+    for(int i = 1; i <= 9; i++) {
+      System.out.print(i + " | ");
+      for(int j = 1; j <= 9; j++) {
+        System.out.printf("%4d", i * j);
+      }
+      System.out.println();
+    }
+  }
+}
