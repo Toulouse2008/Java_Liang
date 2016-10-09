538e468e75020ee241ae08ab083d56950c36617f
Toulouse2008 <frank.du2015@gmail.com>
2016-10-09 16:35:23 -0400

    E5.48 String odd positin

---
+++ chapter5/ProcessString.java
@@ -0,0 +1,23 @@
+/** Exercise 5.48 Process String
+ *  Write a program that prompts the user enter a string and displays the characer
+ *  at odd positions
+ */
+
+import java.util.Scanner;
+
+public class ProcessString {
+  public static void main(String[] args) {
+
+    // Get the input from the user
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter the string: ");
+    String str = input.nextLine();
+
+    // Outpu the chars in odd positions
+    for (int i = 0; i < str.length() / 2; i++) {
+      char ch = str.charAt(2 * i + 1);
+      System.out.print(ch);
+    }
+    System.out.println();
+  }
+}
