6d47953bbd9a710e4ac43a47cf01f679de75aab4
Toulouse2008 <frank.du2015@gmail.com>
2016-10-09 03:31:51 -0400

    E5.19 pattern for 2**n

--- chapter5/Pattern2n.java
+++ chapter5/Pattern2n.java
@@ -19,29 +19,25 @@
    public static void pyramidPattern(int n) {
      int m = 0;
      for (int i = 0; i < n; i++){
-       for (int j = n - i; j > 0; j--){
-         System.out.print("----");
+       for (int j = n - i - 1; j > 0; j--){
+         System.out.print("    ");
        }
        for (int k = 0; k <= i; k++) {
          m = (int)(Math.pow(2, k));
          if (m < 10)
-         System.out.print(m + "---");
+         System.out.print(m + "   ");
          else if (m < 100)
-           System.out.print(m + "--");
-         else if (m < 1000)
-           System.out.print(m + "-");
+           System.out.print(m + "  ");
          else
-           System.out.print(m + "-");
+           System.out.print(m + " ");
        }
        for (int l = i -1; l >= 0; l--) {
          if ((int)Math.pow(2, l) < 10)
-         System.out.print((int)Math.pow(2, l) + "---");
+         System.out.print((int)Math.pow(2, l) + "   ");
          else if ((int)Math.pow(2, l) < 100)
-           System.out.print((int)Math.pow(2, l) + "--");
-         else if ((int)Math.pow(2, l) < 1000)
-           System.out.print((int)Math.pow(2, l) + "-");
+           System.out.print((int)Math.pow(2, l) + "  ");
          else
-           System.out.print((int)Math.pow(2, l) + "");
+           System.out.print((int)Math.pow(2, l) + " ");
        }
        System.out.println();
 
