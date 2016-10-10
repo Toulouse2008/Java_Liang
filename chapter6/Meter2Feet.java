9fa36828c90ac235f106af5ba8020fba74d50fd3
Toulouse2008 <frank.du2015@gmail.com>
2016-10-09 22:47:09 -0400

    Meter 2 feet

---
+++ chapter6/Meter2Feet.java
@@ -0,0 +1,34 @@
+
+public class Meter2Feet{
+  public static void main(String[] args){
+    int feet1,  meters2;
+    double meters1, feet2, feet3, meters3;
+
+    for(feet1 = 1; feet1 <= 10; feet1++){
+      meters1 = MeterFeet.footToMeter((double)feet1);
+      feet2 = (double)feet1;
+
+      System.out.printf("%2.1f\t%1.3f\t|\n", feet2, meters1);
+    }
+    for( meters2 = 20; meters2 <= 65; meters2 += 5){
+      feet3 = MeterFeet.meterToFoot((double)meters2);
+      meters3 = (double)meters2;
+      System.out.printf("\t%2.1f\t%3.3f\n", feet3, meters3);
+    }
+  }
+}
+class MeterFeet{
+  //clas concstructor
+  MeterFeet(double d){
+    double meter = d;
+    double foot = d;
+  }
+  //convert foot to meter
+  public static double footToMeter(double foot){
+    return 0.305 * foot;
+  }
+  //convert meter to foot
+  public static double meterToFoot(double meter){
+    return 3.279 * meter;
+  }
+}
