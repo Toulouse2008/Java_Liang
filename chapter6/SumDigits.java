92f16f79994f8658ea177a71cc21aa705845b419
Toulouse2008 <frank.du2015@gmail.com>
2016-10-09 23:41:49 -0400

    E6.2 Sum the digits

--- chapter6/SumDigits.java
+++ chapter6/SumDigits.java
@@ -1,14 +1,21 @@
-/** output the sum of all the digits till to only one digit */
+/** Exercise 6.2 Sum the digits in an integer
+ *  Write a method that computes the sum of the digits in an integer.
+ *  output the sum of all the digits till to only one digit
+ */
+
 import java.util.Scanner;
 
 public class SumDigits{
   public static void main(String[] args){
+
+    // Get the input from the user
     Scanner input = new Scanner(System.in);
     System.out.println("Enter an integer:");
     long number = input.nextInt();
     System.out.printf("%d\n", (int)sumDigits(number));
   }
-  //add digits till to one digits
+
+  // Define the method that adds digits till to one digits
   public static long sumDigits(long n){
     int temp = 0;
     while(n >= 10){
