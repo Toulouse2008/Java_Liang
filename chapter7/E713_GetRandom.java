9f3ebd494a87c7b27b7df8dc60e087aa039b542c
Toulouse2008 <frank.du2015@gmail.com>
2016-10-12 10:14:15 -0400

    Exercise 7.13 confused??

---
+++ chapter7/E713_GetRandom.java
@@ -0,0 +1,11 @@
+/** Exercise 7.13 Random number chooser
+ *  Write a method that returns a random number between 1 and 54, excluding
+ *  the numbers passed in the argument. The method header is specified as
+ *  follows:
+ *  public static int getRandom(int...numbers)
+ */
+
+public static int getRandom(int...numbers) {
+  random = 1 + (int)(Math.random() * 55)
+  return random;
+}
